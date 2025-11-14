70af2231141948d8338464217fcd917fd69c9e60
MonsieurClavett <alexmarcouiller9@gmail.com>
2025-11-14 10:41:22 -0500

    DONE

--- GestionBanque.Tests/ClientTest.cs
+++ GestionBanque.Tests/ClientTest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GestionBanque.Models;
-
+//
 namespace GestionBanque.Tests
 {
     public class ClientTest
--- GestionBanque/Models/Client.cs
+++ GestionBanque/Models/Client.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
-
+//
 namespace GestionBanque.Models
 {
     public class Client : INotifyPropertyChanged
